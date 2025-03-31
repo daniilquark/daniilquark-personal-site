f3497f82f14aef034ae53e8f9bd25ebb5a723274
daniilquark <daniilquark@gmail.com>
2025-03-31 17:40:33 +0000

    Revert to zero

--- pages/contact/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	setTimeout(function() {
-		document.getElementById("popup").style.display = "block";
-	}, 500);
-};
-
-function closePopup() {
-	document.getElementById("popup").style.display = "none";
-}
